2b4ccf1476b484441cba2be8039c7f0bbfb16cb9
Cabia Rangris <cab@cab.moe>
2023-12-06 21:23:58 +0400

    circles

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <SPI.h>
 
 const int OE = 13;
 const int A = 13;
@@ -7,6 +8,9 @@ const int R = 12;
 const int CLK = 2;
 const int SCLK = 4;
 
+byte buf[512];
+char sprintbuf[128];
+
 #define dw digitalWrite
 #define unsugarC(c)                  \
     {                                \
@@ -14,8 +18,15 @@ const int SCLK = 4;
         dw(B, ((c & 1) ^ (c >> 1))); \
     }
 
+int coordToP(int x, int y)
+{
+    return (((31 - x) >> 3) << 7) | ((y >> 2) << 5) | ((7 - x & 7) << 2) | (y & 3);
+}
+
 void setup()
 {
+    Serial.begin(9600);
+    
     pinMode(OE, OUTPUT);
     pinMode(A, OUTPUT);
     pinMode(B, OUTPUT);
@@ -23,74 +34,55 @@ void setup()
     pinMode(CLK, OUTPUT);
     pinMode(SCLK, OUTPUT);
     dw(OE, 0);
-}
-
-void reset()
-{
-    for (int a = 1; a != 4; a <<= 1)
+    for (int x = 0; x < 32; x++)
     {
-        dw(A, a >> 1);
-        for (int b = 1; b != 4; b <<= 1)
+        for (int y = 0; y < 16; y++)
         {
-            dw(B, b >> 1);
-            for (int p = 0; p < 128; p++)
+            double len = sqrt((x - 5) * (x - 5) + (y - 5) * (y - 5));
+            if (len <= 10)
             {
-                dw(R, 1);
-                dw(CLK, 0);
-                dw(CLK, 1);
+                int br = (10.0 - len) * 4;
+                if (br > 4)
+                    br = 4;
+                buf[coordToP(x, y)] = br;
             }
-            dw(SCLK, 0);
-            dw(SCLK, 1);
         }
     }
 }
-
+#ifdef FRAMETIMES
 int clock = 0;
+#endif
 void loop()
 {
+#ifdef FRAMETIMES
     clock += 1;
-    clock %= 1 << 11;
-    // Write a thing
-    for (int c = 0; c < 4; c++)
+    clock %= 100;
+    long time = micros();
+#endif
+
     {
-        unsugarC(c)
+        for (int c = 0; c < 4; c++)
         {
-            for (int p = 0; p < 128; p++)
+            unsugarC(c);
             {
-                dw(R, ((p << 2) | c) < (clock >> 0));
-                dw(CLK, 0);
-                dw(CLK, 1);
+                for (int p = 0; p < 128; p++)
+                {
+                    int px = ((p << 2) | c);
+                    dw(R, !(buf[px]));
+                    dw(CLK, 0);
+                    dw(CLK, 1);
+                }
+                dw(SCLK, 0);
+                dw(SCLK, 1);
             }
-            dw(SCLK, 0);
-            dw(SCLK, 1);
         }
     }
-    for (int c = 0; c < 4; c++)
+#ifdef FRAMETIMES
+    long diff = micros() - time;
+    if (clock == 0)
     {
-        unsugarC(c)
-        {
-            for (int p = 0; p < 128; p++)
-            {
-                dw(R, ((p << 2) | c) < (clock >> 1));
-                dw(CLK, 0);
-                dw(CLK, 1);
-            }
-            dw(SCLK, 0);
-            dw(SCLK, 1);
-        }
+        sprintf(sprintbuf, "Frame in %d", diff);
+        Serial.println(sprintbuf);
     }
-    for (int c = 0; c < 4; c++)
-    {
-        unsugarC(c)
-        {
-            for (int p = 0; p < 128; p++)
-            {
-                dw(R, ((p << 2) | c) < (clock >> 4));
-                dw(CLK, 0);
-                dw(CLK, 1);
-            }
-            dw(SCLK, 0);
-            dw(SCLK, 1);
-        }
-    }
+#endif
 }
